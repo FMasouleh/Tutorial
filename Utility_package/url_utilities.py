0c237a5fe99ba513cdf9f07994dbfc36b288647d
FMasouleh <Shabnam66>
2018-02-28 14:13:38 +1300

    initial commit

---
+++ Utility_package/url_utilities.py
@@ -0,0 +1,17 @@
+def load_urls_from_file(file_path: str):
+    # todo: add the code needed to read the terxt file with the urls in it
+    pass
+
+
+def load_page(url: str):
+
+
+# TODO: add the code that reads the url
+pass
+
+
+def scrap_page(page_content: str):
+
+
+# TODO: analyze the text
+pass
