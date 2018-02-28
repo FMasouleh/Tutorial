9fb8a55e435cedbb32522d57d7073862ee73c309
FMasouleh <Shabnam66>
2018-02-28 15:05:28 +1300

    finished real url_utilities

--- Utility_package/url_utilities.py
+++ Utility_package/url_utilities.py
@@ -9,16 +9,14 @@ def load_urls_from_file(file_path: str):
         with open(file_path) as f:
             content = f.readline()
             return content
-        except FileNotFoundError:
+    except FileNotFoundError:
         print("the file " + file_path + "could not be found")
         exit(2)
 
 
 def load_page(url: str):
     response = urlopen(url)
-
-
-html = response.read().decode('utf-8')
+    html = response.read().decode('utf-8')
 
 
 def scrape_page(page_contents: str):
@@ -31,33 +29,32 @@ def scrape_page(page_contents: str):
     lines = (line.strip() for line in text.splitlines())
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
 
+    text = ' '.join(chunk for chunk in chunks if chunk)
+    plain_text = ''.join(filter(lambda x: x in string.printable, text))
 
-text = ' '.join(chunk for chunk in chunks if chunk)
-plain_text = ''.join(filter(lambda x: x in string.printable, text))
+    clean_words = []
 
-clean_words = []
-
-words = plain_text.split(" ")
-for word in words:
+    words = plain_text.split(" ")
+    for word in words:
     clean = True
 
-# no punctuation
-for punctuation_marks in string.punctuation:
+     # no punctuation
+ for punctuation_marks in string.punctuation:
     if punctuation_marks in word:
-        clean = False
-
-# no numbers
-if any(char.isdigit() for char in word):
     clean = False
 
-# at least two characters but no more than 10
-if len(word) < 2 or len(word) > 10:
+   # no numbers
+    if any(char.isdigit() for char in word):
     clean = False
 
-if not re.match(r'^\w+$', word):
+    # at least two characters but no more than 10
+    if len(word) < 2 or len(word) > 10:
     clean = False
 
-if clean:
+     if not re.match(r'^\w+$', word):
+    clean = False
+
+    if clean:
     try:
         clean_words.append(word.lower())
     except UnicodeEncodeError:
